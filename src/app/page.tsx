c6696f8ab962a45868c03c0325b860e86709ef1f
Junior Alves <jrnalves@gmail.com>
2026-01-25 13:12:10 -0300

    feat: adicionar seção de voluntários
    - Criado o componente `Volunteers.tsx` para exibição dos membros.
    - Integrada a seção de voluntários na página inicial (`src/app/page.tsx`).
    - Adicionadas novas imagens de perfil para os voluntários em `public/images/voluntarios/`.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import GetInvolved from '@/components/Get-Involved'
 import Header from '@/components/Header'
 import Mission from '@/components/Mission'
 import SocialImpact from '@/components/Social-Impact'
+import Volunteers from '@/components/Volunteers'
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Mission />
         <Activities />
         <SocialImpact />
+        <Volunteers />
       </main>
     </>
   )
