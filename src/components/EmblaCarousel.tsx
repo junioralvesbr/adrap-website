dc35c869f9c5d72b08b14f06df6645a0796526b4
Junior Alves <jrnalves@gmail.com>
2026-02-09 01:28:57 -0300

    - Trocado o nome dos novos voluntarios - diminuido o tamanho dos cads, foto e texto do carrossel

--- src/components/EmblaCarousel.tsx
+++ src/components/EmblaCarousel.tsx
@@ -32,31 +32,33 @@ export default function EmblaCarousel({
   return (
     <section className='relative'>
       <div className='overflow-hidden' ref={emblaRef}>
-        <div className='flex py-8'>
+        <div className='flex py-6 md:py-8'>
           {slides.map((slide, index) => (
             <div
               key={index}
-              className='shrink-0 grow-0 basis-3xs cursor-grab pl-4 md:basis-1/5'
+              className='cursor-grab pl-4 md:shrink-0 md:grow-0 md:basis-1/5'
             >
               <Card
                 key={index}
-                className='bg-card group min-h-96 overflow-hidden border-0 py-0 shadow-lg transition-all duration-300 hover:shadow-xl'
+                className='bg-card group min-h-62 overflow-hidden border-0 py-0 shadow-lg transition-all duration-300 hover:shadow-xl md:h-full md:w-full'
               >
-                <div className='relative aspect-square overflow-hidden'>
+                <div className='relative aspect-square h-42 overflow-hidden lg:h-full'>
                   <Image
                     src={slide.image || '/placeholder.svg'}
                     alt={slide.name}
                     fill
                     className='object-cover object-top transition-transform duration-500 group-hover:scale-110'
-                    sizes='(max-width: 768px) 50vw, (max-width: 1200px) 25vw, 20vw'
+                    sizes='100vw'
                   />
                   <div className='from-foreground/80 absolute inset-0 bg-linear-to-t via-transparent to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100' />
                 </div>
-                <CardContent className='p-5 text-center'>
-                  <h3 className='text-foreground text-lg font-bold'>
+                <CardContent className='p-2 text-center lg:p-5'>
+                  <h3 className='text-foreground text-base font-bold'>
                     {slide.name}
                   </h3>
-                  <p className='text-muted-foreground text-sm'>{slide.role}</p>
+                  <p className='text-muted-foreground text-xs md:text-sm'>
+                    {slide.role}
+                  </p>
                 </CardContent>
               </Card>
             </div>
